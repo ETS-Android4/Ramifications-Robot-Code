8378e8412ae985b2ec4e1fc4591504a5fd956fe0
Lucas McCarty <ltmccarty01@gmail.com>
2020-12-20 14:55:24 -0600

    Troubleshooting autonomous driving

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/MoveSquare.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
+
+import java.util.List;
+
+@Autonomous(name = "movesquare", group = "Concept")
+//@Disabled
+public class MoveSquare extends LinearOpMode {
+    private Robot robot;
+    private MecanumDrive mecanumDrive;
+
+
+
+
+    @Override
+    public void runOpMode() {
+        this.robot = new Robot(hardwareMap);
+        this.mecanumDrive = (MecanumDrive) this.robot.getDrivetrain();
+
+        waitForStart();
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(),1,0);
+        sleep(500);
+        this.mecanumDrive.stopMoving();
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),1,0);
+        sleep(500);
+        this.mecanumDrive.stopMoving();
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.DOWN.angle(),1,0);
+        sleep(500);
+        this.mecanumDrive.stopMoving();
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.LEFT.angle(),1,0);
+        sleep(500);
+        this.mecanumDrive.stopMoving();
+
+    }
+
+}

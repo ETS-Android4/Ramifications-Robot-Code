5214f72b766e018cbd3134671fdf597d47c2f21c
Lucas McCarty <ltmccarty01@gmail.com>
2021-02-02 18:08:31 -0600

    Further auto changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedCenter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedCenter.java
@@ -70,7 +70,7 @@ public class AutoRedCenter extends LinearOpMode {
             // (typically 1.78 or 16/9).
 
             // Uncomment the following line if you want to adjust the magnification and/or the aspect ratio of the input images.
-            tfod.setZoom(2.5, 1.78);
+            //tfod.setZoom(2.5, 1.78);
         }
 
         this.robot = new Robot(hardwareMap);

99ba9dc374daffd11959fa9ebd4d8caabeae9f77
Lucas McCarty <ltmccarty01@gmail.com>
2021-02-11 13:58:40 -0600

    Fixing and Expanding Simplified Movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SimplifiedMovement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/SimplifiedMovement.java
@@ -1,35 +1,43 @@
 package org.firstinspires.ftc.teamcode.Autonomous;
-
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.teamcode.robotplus.autonomous.TimeOffsetVoltage;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
 
 public class SimplifiedMovement {
-    private void Forward(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage) {
-        mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(),1,0);
-        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, 150));
+    private void Forward(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(),velocity,0);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
         mecanumDrive.stopMoving();
     }
 
-    private void Backward(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage) {
-        mecanumDrive.complexDrive(MecanumDrive.Direction.DOWN.angle(),1,0);
-        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, 150));
+    private void Backward(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.DOWN.angle(),velocity,0);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
         mecanumDrive.stopMoving();
     }
 
-    private void Right(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage) {
-        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),0,1);
-        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, 150));
+    private void Right(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),0,velocity);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
         mecanumDrive.stopMoving();
     }
 
-    private void Left(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage) {
-        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),0,-1);
-        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, 150));
+    private void Left(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),0,-velocity);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
+        mecanumDrive.stopMoving();
+    }
+
+    private void TurnRight(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(), velocity,0);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
+        mecanumDrive.stopMoving();
+    }
+
+    private void TurnLeft(LinearOpMode lop, Robot robot, MecanumDrive mecanumDrive, double voltage, double distance, double velocity) {
+        mecanumDrive.complexDrive(MecanumDrive.Direction.RIGHT.angle(),-velocity,0);
+        lop.sleep(TimeOffsetVoltage.calculateDistance(voltage, distance));
         mecanumDrive.stopMoving();
     }
 }

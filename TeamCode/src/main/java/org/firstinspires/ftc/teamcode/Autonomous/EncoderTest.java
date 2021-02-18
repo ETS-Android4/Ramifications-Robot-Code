10246e4c728e8208e6c44797168abbfbac1001ed
Sebastian Neumann <sneumann@micds.org>
2021-02-18 14:13:56 -0600

    Trying out Encoders

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/EncoderTest.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
+import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
+
+public class EncoderTest extends LinearOpMode {
+    private Robot robot;
+    private DcMotorEx shooter1;
+    private DcMotorEx shooter2;
+    private CRServo hopperpush;
+    private MecanumDrive mecanumDrive;
+    private int motorVelocity = 2200;
+
+    @Override
+    public void runOpMode() {
+        this.robot = new Robot(hardwareMap);
+        this.shooter1 = (DcMotorEx) hardwareMap.get(DcMotor.class, "shooter1");
+        this.shooter1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.shooter2 = (DcMotorEx) hardwareMap.get(DcMotor.class, "shooter2");
+        this.shooter2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        this.hopperpush = hardwareMap.get(CRServo.class, "hopperpush");
+
+
+
+        waitForStart();
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(),1,0);
+        sleep(1900);
+        this.mecanumDrive.stopMoving();
+
+        this.shooter1.setVelocity(motorVelocity);
+        this.shooter2.setVelocity(motorVelocity*0.75);
+        sleep(500);
+        this.hopperpush.setPower(-0.5);
+        sleep(3000);
+        this.shooter1.setVelocity(0);
+        this.shooter2.setVelocity(0);
+        this.hopperpush.setPower(0);
+
+        this.mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(), 1, 0);
+        sleep(350);
+        this.mecanumDrive.stopMoving();
+
+    }
+}

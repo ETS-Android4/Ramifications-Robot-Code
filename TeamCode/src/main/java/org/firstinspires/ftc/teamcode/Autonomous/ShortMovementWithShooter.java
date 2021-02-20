8dc128b1ff0b1f01a175364bc20cc9127f05a15e
skrrt <bcosmopoulos@micds.org>
2021-02-20 11:16:26 -0600

    Stuff for GAMERZZZ.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ShortMovementWithShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/ShortMovementWithShooter.java
@@ -4,6 +4,8 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.robotplus.hardware.MecanumDrive;
 import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
@@ -13,30 +15,39 @@ import org.firstinspires.ftc.teamcode.robotplus.hardware.Robot;
 public class ShortMovementWithShooter extends LinearOpMode {
     private Robot robot;
     private MecanumDrive mecanumDrive;
-    private DcMotor shooter1;
+    private DcMotorEx shooter1;
     private DcMotor shooter2;
     private CRServo hopperpush;
+    private Servo claw;
+    private DcMotor arm;
 
     @Override
     public void runOpMode() {
         this.robot = new Robot(hardwareMap);
         this.mecanumDrive = (MecanumDrive) this.robot.getDrivetrain();
-        this.shooter1 = hardwareMap.get(DcMotor.class, "shooter1");
+        this.shooter1 = (DcMotorEx) hardwareMap.get(DcMotor.class, "shooter1");
         this.shooter2 = hardwareMap.get(DcMotor.class, "shooter2");
         this.hopperpush = hardwareMap.get(CRServo.class, "hopperpush");
+        this.claw = hardwareMap.get(Servo.class, "claw");
+        claw.setPosition(0.63);
+        this.arm = hardwareMap.get(DcMotor.class, "arm");
 
         waitForStart();
+
+        arm.setPower(-0.2); // TODO: 2/11/2021 might need to reverse again
 
         this.mecanumDrive.complexDrive(MecanumDrive.Direction.UP.angle(),1,0);
         sleep(1900);
         this.mecanumDrive.stopMoving();
 
-        this.shooter1.setPower(1.0);
+        arm.setPower(0); // TODO: 2/11/2021 might need to reverse again
+
+        this.shooter1.setVelocity(2300);
         this.shooter2.setPower(0.75);
         sleep(500);
         this.hopperpush.setPower(-0.5);
         sleep(3000);
-        this.shooter1.setPower(0);
+        this.shooter1.setVelocity(0);
         this.shooter2.setPower(0);
         this.hopperpush.setPower(0);
 

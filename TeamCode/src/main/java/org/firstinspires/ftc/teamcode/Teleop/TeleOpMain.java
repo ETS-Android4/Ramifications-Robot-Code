c5f785501d478cc1d6c9164a6cdbf3a02f317bdb
Henry Clay <hclay@micds.org>
2021-12-13 17:04:42 -0600

    Add intake/outtake/carousel to teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleOpMain.java
@@ -1,9 +1,17 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
+import static java.lang.Thread.sleep;
+
+import com.qualcomm.hardware.motors.RevRoboticsCoreHexMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
 
 import org.firstinspires.ftc.micdsrobotics.robotplus.gamepadwrapper.Controller;
+import org.firstinspires.ftc.micdsrobotics.robotplus.hardware.Motor;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.MotorPower;
 import org.firstinspires.ftc.teamcode.TicondeRobot;
@@ -11,11 +19,26 @@ import org.firstinspires.ftc.teamcode.TicondeRobot;
 @TeleOp(name = "TeleOP Main V2", group = "Basic")
 public class TeleOpMain extends OpMode {
     private TicondeRobot robot = new TicondeRobot();
+    private Servo intake;
+    private Servo outtake;
+    private DcMotor outtakeRaise;
+    private DcMotor spinnyWheel;
+    private double intakePosition;
+    private double outtakePosition;
+    private double outtakeRaisePosition;
 
 
     @Override
     public void init() {
         this.robot.initHardware(hardwareMap);
+        this.intake = hardwareMap.get(Servo.class, "intake");
+        this.outtake = hardwareMap.get(Servo.class, "outtake");
+        this.outtakeRaise = hardwareMap.get(DcMotor.class, "outtakeRaise");
+        this.spinnyWheel = hardwareMap.get(DcMotor.class, "spinnyWheel");
+        intake.setPosition(0);
+        outtake.setPosition(0);
+
+
 
     }
 
@@ -26,5 +49,44 @@ public class TeleOpMain extends OpMode {
         // these two lines handle omnidirectional movement and report the motor values to telemetry
         MotorPower power = this.robot.move(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         telemetry.addLine(power.toString());
+
+        //intake
+        if (gamepad1.x) {
+            intake.setPosition(1);
+            while(intake.getPosition() != 1) {
+
+            }
+            intake.setPosition(0);
+        }
+        //outtake
+        if (gamepad1.y) {
+            outtake.setPosition(1);
+            while(outtake.getPosition() != 1) {
+
+            }
+            outtake.setPosition(0);
+        }
+        //Outtake up
+        if (gamepad1.dpad_up) {
+            outtakeRaise.setPower(1);
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        //Outtake down
+        if (gamepad1.dpad_down) {
+            outtakeRaise.setPower(-1);
+            try{
+                sleep(1000);
+            } catch (InterruptedException f) {
+                f.printStackTrace();
+            }
+        }
+        //Carousel wheel
+        if (gamepad1.right_trigger>0) {
+            spinnyWheel.setPower(gamepad1.right_trigger);
+        }
     }
-}//can I commit
+}

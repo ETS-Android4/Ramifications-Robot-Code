bc16b25b01b44b99e4aaf8973126acc5eba2385b
Henry Clay <hclay@micds.org>
2021-12-13 19:54:09 -0600

    Added second outtake motor
    Final adjustment for practice awaiting Sam's encoder work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/TeleOpMain.java
@@ -23,9 +23,8 @@ public class TeleOpMain extends OpMode {
     private Servo outtake;
     private DcMotor outtakeRaise;
     private DcMotor spinnyWheel;
-    private double intakePosition;
-    private double outtakePosition;
-    private double outtakeRaisePosition;
+    private DcMotor outtakeLower;
+    private int outtakeRaisePosition;
 
 
     @Override
@@ -35,8 +34,10 @@ public class TeleOpMain extends OpMode {
         this.outtake = hardwareMap.get(Servo.class, "outtake");
         this.outtakeRaise = hardwareMap.get(DcMotor.class, "outtakeRaise");
         this.spinnyWheel = hardwareMap.get(DcMotor.class, "spinnyWheel");
+        this.outtakeLower = hardwareMap.get(DcMotor.class, "outtakeLower");
         intake.setPosition(0);
         outtake.setPosition(0);
+        int outtakeRaisePosition = 1;
 
 
 
@@ -67,22 +68,31 @@ public class TeleOpMain extends OpMode {
             outtake.setPosition(0);
         }
         //Outtake up
-        if (gamepad1.dpad_up) {
+        if (gamepad1.dpad_up || outtakeRaisePosition != 2) {
             outtakeRaise.setPower(1);
+            outtakeLower.setPower(-1);
+
             try {
-                sleep(1000);
+                sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            outtakeRaise.setPower(0);
+            outtakeLower.setPower(0);
+            outtakeRaisePosition = outtakeRaisePosition + 1;
         }
         //Outtake down
-        if (gamepad1.dpad_down) {
+        if (gamepad1.dpad_down || outtakeRaisePosition != 0) {
             outtakeRaise.setPower(-1);
+            outtakeLower.setPower(1);
             try{
-                sleep(1000);
+                sleep(500);
             } catch (InterruptedException f) {
                 f.printStackTrace();
             }
+            outtakeRaise.setPower(0);
+            outtakeLower.setPower(0);
+            outtakeRaisePosition = outtakeRaisePosition - 1;
         }
         //Carousel wheel
         if (gamepad1.right_trigger>0) {

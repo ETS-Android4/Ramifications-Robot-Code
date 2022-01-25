0c7060014e8407aae920fdbc5b8ecba294de0da4
Sam Baumohl <mail@sambaumohl.me>
2022-01-24 19:19:45 -0600

    We gotta start thinking about Vuforia
    Vuforia License

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/license_example.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+// You aren't supposed to upload your Vuforia License to Github.
+// Add your key here and copy and paste this file's contents into a new file
+// name that file "license.java".
+public class license_example {
+    public static final String VUFORIA_KEY = "";
+}
